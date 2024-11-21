c6b286a11c7f21fe2f04bb6a0ce73055f3bf8fe1
hgb926 <hgb926@naver.com>
2024-11-21 17:48:28 +0900

    [Chore] host-config 설정

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -1,1 +1,23 @@
-//
+const LOCAL_PORT = 8888;
+const LOCAL_FRONT = 3000;
+
+const clientHostName = window.location.hostname;
+
+let backendHostName;
+let frontendHostName;
+
+if (clientHostName === "localhost") {
+    backendHostName = "http://localhost:" + LOCAL_PORT;
+    frontendHostName = "http://localhost:" + LOCAL_FRONT;
+} else {
+    // backendHostName = 'http://43.203.105.27:8888';
+    // frontendHostName = "http://doggle.kr";
+}
+
+const API_BASE_URL = backendHostName;
+const APP_BASE_URL = frontendHostName;
+
+// const ROOM = "/room";
+
+export const BASE_URL = API_BASE_URL;
+// export const ROOM_URL = API_BASE_URL + ROOM;

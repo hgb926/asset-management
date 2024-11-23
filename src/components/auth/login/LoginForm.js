6d61a0345a89457ae0807c0172505e116c6e2b46
hgb926 <hgb926@naver.com>
2024-11-23 14:03:58 +0900

    [Design] 로그인 컴포넌트 완성

---
+++ src/components/auth/login/LoginForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from '../../../styles/auth/LoginForm.module.scss';
+
+const LoginForm = () => {
+    return (
+        <div className={styles.wrap}>
+            <h1 className={styles.h1}>Asset Management</h1>
+            <form className={styles.form}>
+                <div className={styles.inputWrap}>
+                    <label className={styles.label}>이메일</label>
+                    <input className={styles.input} type="email" placeholder="이메일을 입력하세요" />
+                </div>
+                <div className={styles.inputWrap}>
+                    <label className={styles.label}>비밀번호</label>
+                    <input className={styles.input} type="password" placeholder="비밀번호를 입력하세요" />
+                </div>
+                <div className={styles.buttons}>
+                    <button className={styles.loginButton} type="submit">로그인</button>
+                    <button className={styles.signupButton} type="button">회원가입</button>
+                </div>
+            </form>
+            <p className={styles.findPw}>비밀번호 찾기</p>
+        </div>
+    );
+};
+
+export default LoginForm;

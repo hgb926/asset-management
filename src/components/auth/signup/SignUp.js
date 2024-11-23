6d4c2f189737386c745b898c08633db1b0df02d5
hgb926 <hgb926@naver.com>
2024-11-23 15:18:43 +0900

    [Design] 회원가입 Form 3차 디자인
    input, span 사이의 여백 조정

--- src/components/auth/signup/SignUp.js
+++ src/components/auth/signup/SignUp.js
@@ -7,18 +7,18 @@ const SignUp = () => {
         <div className={styles.wrap}>
             <h1 className={styles.h1}>회원가입</h1>
             <div className={styles.inputWrap}>
-                <p className={styles.text}>이메일</p>
+                <span className={styles.text}>이메일</span>
                 <div className={styles.emailVerification}>
                     <input className={styles.input} type="email" placeholder="이메일을 입력하세요" />
                     <button className={styles.button}>인증코드 전송</button>
                 </div>
-                <p className={styles.text}>인증코드</p>
+                <span className={styles.text}>인증코드</span>
                 <input className={styles.input} type="text" placeholder="인증코드를 입력하세요" />
-                <p className={styles.text}>비밀번호</p>
+                <span className={styles.text}>비밀번호</span>
                 <input className={styles.input} type="password" placeholder="비밀번호를 입력하세요" />
-                <p className={styles.text}>비밀번호 확인</p>
+                <span className={styles.text}>비밀번호 확인</span>
                 <input className={styles.input} type="password" placeholder="비밀번호를 다시 입력하세요" />
-                <p className={styles.text}>닉네임</p>
+                <span className={styles.text}>닉네임</span>
                 <input className={styles.input} type="text" placeholder="닉네임을 입력하세요" />
             </div>
             <div className={styles.flex}>

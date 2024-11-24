f32b5dc497978217adcc0add9481a8b705bb36e5
hgb926 <hgb926@naver.com>
2024-11-24 14:10:02 +0900

    [Design] header 유저 닉네임 렌더링

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from '../../styles/main/Header.module.scss'
 import {IoSettingsOutline} from "react-icons/io5";
+import {useSelector} from "react-redux";
+import {userInfoActions} from "../store/user/UserInfoSlice";
 
 const Header = () => {
+
+    const userData = useSelector(state => state.userInfo.userData) || "user";
+    console.log(userData)
+
+
     return (
         <div className={styles.container}>
             <div className={styles.left}></div>
             <h1 className={styles.h1}>Asset Management</h1>
             <div className={styles.right}>
-                <div className={styles.welcome}>user님, 환영합니다 </div>
+                <div className={styles.welcome}>{userData.nickname}님, 환영합니다 </div>
                 <IoSettingsOutline className={styles.settings} />
             </div>
         </div>

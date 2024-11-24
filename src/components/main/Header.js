ef7060f7053b30262afed1460efec1defbea9abc
hgb926 <hgb926@naver.com>
2024-11-24 14:06:58 +0900

    [Design] header 기본 레이아웃

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from '../../styles/main/Header.module.scss'
+import {IoSettingsOutline} from "react-icons/io5";
 
 const Header = () => {
     return (
         <div className={styles.container}>
-            Header
+            <div className={styles.left}></div>
+            <h1 className={styles.h1}>Asset Management</h1>
+            <div className={styles.right}>
+                <div className={styles.welcome}>user님, 환영합니다 </div>
+                <IoSettingsOutline className={styles.settings} />
+            </div>
         </div>
     );
 };

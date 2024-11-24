ca6e175ff39f0736d2427089ae2bf9e0d1ba4b28
hgb926 <hgb926@naver.com>
2024-11-24 22:37:42 +0900

    [Init] Analysis, Challenge, Goal 컴포넌트 생성

---
+++ src/components/main/challenge/Challenge.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Challenge = () => {
+    return (
+        <div>
+            Challenge
+        </div>
+    );
+};
+
+export default Challenge;
---
+++ src/components/main/goal/Goal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Goal = () => {
+    return (
+        <div>
+            Goal
+        </div>
+    );
+};
+
+export default Goal;

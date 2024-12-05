d2a66f9b2d82c8c52c40adc7e29182a7ee253b98
hgb926 <hgb926@naver.com>
2024-12-05 15:51:09 +0900

    [Feat] 모달 닫기 버튼 활성화

--- src/components/main/goal/AddGoalBtn.js
+++ src/components/main/goal/AddGoalBtn.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from '../../../styles/goal/AddGoalBtn.module.scss';
 import AddGoalModal from "../../../modal/AddGoalModal";
 
 const AddGoalBtn = () => {
+
+    const [openModal, setOpenModal] = useState(false)
+
+    const modalHandler = (flag) => {
+        setOpenModal(flag)
+    }
+
+    // 전역적으로 모달 닫기, 오버레이
     return (
         <>
-            <div className={styles.addGoalBtnWrap}>
+            <div className={styles.addGoalBtnWrap} onClick={() => setOpenModal(true)}>
                 목표 추가하기
             </div>
-            <AddGoalModal />
+            {openModal && <AddGoalModal modalHandler={modalHandler}/>}
         </>
     );
 };
--- src/modal/AddGoalModal.js
+++ src/modal/AddGoalModal.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from '../styles/goal/AddGoalModal.module.scss'
 
-const AddGoalModal = () => {
-
+const AddGoalModal = ({ modalHandler }) => {
 
 
     return (
         <div className={styles.modalWrap}>
             <h1 className={styles.h1}>새로운 목표를 설정해주세요!</h1>
-            <span className={styles.closeButton} >
-                                ×
-                            </span>
+            <span className={styles.closeButton} onClick={() => modalHandler(false)}>×</span>
         </div>
     );
 };

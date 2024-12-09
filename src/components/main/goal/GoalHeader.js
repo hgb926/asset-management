01601563fc9b6f73c234ae81d4bf437dc5ee61cb
hgb926 <hgb926@naver.com>
2024-12-09 20:12:53 +0900

    [Init] Goal 관련 scss파일 초기화

--- src/components/main/goal/GoalHeader.js
+++ src/components/main/goal/GoalHeader.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import AddGoalBtn from "./AddGoalBtn";
 import styles from '../../../styles/goal/GoalHeader.module.scss'
+import {useSelector} from "react-redux";
 
 const GoalHeader = () => {
+
+    const userData = useSelector(state => state.userInfo.userData);
+
+    for (let i = 0; i < userData.goalList.length; i++) {
+        console.log(userData.goalList[i])
+    }
+
     return (
         <div className={styles.headerContainer}>
+
+
 
             <AddGoalBtn/>
         </div>

33ab20399ad2a64ec35a514abe3e1a40445f434d
hgb926 <hgb926@naver.com>
2024-12-26 20:23:09 +0900

    [UI] Changed GNB UI
    float, background color white

---
+++ src/components/main/challenge/Boards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../../styles/challenge/Boards.module.scss'
+
+const Boards = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Boards;
--- src/components/main/challenge/Challenge.js
+++ src/components/main/challenge/Challenge.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '../../../styles/challenge/Challenge.module.scss';
 import ChallengeHeader from "./ChallengeHeader";
+import Boards from "./Boards";
 
 const Challenge = () => {
     return (
         <div className={styles.wrap}>
             <ChallengeHeader/>
+            <Boards/>
         </div>
     );
 };

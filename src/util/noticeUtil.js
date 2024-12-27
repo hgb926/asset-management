b5869fb5ceb2267fa9766b04a41e7949975212f3
hgb926 <hgb926@naver.com>
2024-12-27 21:09:57 +0900

    [Feat] 알림 작성 유틸 함수 생성

---
+++ src/util/noticeUtil.js
@@ -0,0 +1,19 @@
+import {NOTICE_URL} from "../config/host-config";
+
+
+export const addNotice = async (userId, message) => {
+    const payload = {
+        userId,
+        message
+    }
+
+    const response = await fetch(`${NOTICE_URL}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+    });
+    console.log(response)
+    if (response.ok) {
+        return await response.json();
+    }
+}

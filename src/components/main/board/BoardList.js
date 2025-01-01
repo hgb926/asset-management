621844d19d99cfe50796d4d2937caf72eb640af7
hgb926 <hgb926@naver.com>
2025-01-01 21:29:09 +0900

    [UI] board 검색결과 없을때 UI

--- src/components/main/board/BoardList.js
+++ src/components/main/board/BoardList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from '../../../styles/board/BoardList.module.scss';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {
     MdKeyboardArrowLeft,
     MdKeyboardArrowRight,
     MdKeyboardDoubleArrowLeft,
     MdKeyboardDoubleArrowRight
 } from "react-icons/md";
-import { formatRelativeTime } from '../../../util/timeFormater';
+import {formatRelativeTime} from '../../../util/timeFormater';
 
-const BoardList = ({ boardList, currentPage, totalPages, isLastPage, changePage }) => {
+const BoardList = ({boardList, currentPage, totalPages, isLastPage, changePage}) => {
     const now = new Date();
 
     // 페이지네이션 범위 계산
@@ -21,68 +21,70 @@ const BoardList = ({ boardList, currentPage, totalPages, isLastPage, changePage 
         if (endPage > totalPages) {
             endPage = totalPages;
         }
-        return Array.from({ length: endPage - startPage }, (_, index) => startPage + index);
+        return Array.from({length: endPage - startPage}, (_, index) => startPage + index);
     };
 
     return (
         <>
-            <div className={styles.boardWrap}>
-                <div className={styles.boardList}>
-                    <div className={styles.header}>
-                        <span>번호</span>
-                        <span>제목</span>
-                        <span>작성자</span>
-                        <span>작성일</span>
-                        <span>조회수</span>
+        {boardList.length > 0 ? (
+              <div className={styles.boardWrap}>
+        <div className={styles.boardList}>
+            <div className={styles.header}>
+                <span>번호</span>
+                <span>제목</span>
+                <span>작성자</span>
+                <span>작성일</span>
+                <span>조회수</span>
+            </div>
+            {boardList.map((board) => {
+                const diffInMs = now - new Date(board.createdAt);
+                return (
+                    <div key={board.id} className={styles.boardItem}>
+                        <span className={styles.itemId}>{board.id}</span>
+                        <Link to={`/board/${board.id}`} className={styles.itemTitle}>
+                            {board.title} ({board.replyCount})
+                        </Link>
+                        <span className={styles.itemAuthor}>{board.author}</span>
+                        <span className={styles.itemDate}>{formatRelativeTime(diffInMs)}</span>
+                        <span className={styles.itemViewCount}>{board.viewCount}</span>
                     </div>
-                    {boardList.map((board) => {
-                        const diffInMs = now - new Date(board.createdAt);
-                        return (
-                            <div key={board.id} className={styles.boardItem}>
-                                <span className={styles.itemId}>{board.id}</span>
-                                <Link to={`/board/${board.id}`} className={styles.itemTitle}>
-                                    {board.title} ({board.replyCount})
-                                </Link>
-                                <span className={styles.itemAuthor}>{board.author}</span>
-                                <span className={styles.itemDate}>{formatRelativeTime(diffInMs)}</span>
-                                <span className={styles.itemViewCount}>{board.viewCount}</span>
-                            </div>
-                        );
-                    })}
+                );
+            })}
+        </div>
+
+        {/* 페이지네이션 */}
+        <div className={styles.footer}>
+            <div className={styles.btnWrap}>
+                <div onClick={() => changePage(0)} disabled={currentPage === 0}>
+                    <MdKeyboardDoubleArrowLeft/>
+                </div>
+                <div onClick={() => changePage(currentPage - 1)} disabled={currentPage === 0}>
+                    <MdKeyboardArrowLeft/>
                 </div>
 
-                {/* 페이지네이션 */}
-                <div className={styles.footer}>
-                    <div className={styles.btnWrap}>
-                        <div onClick={() => changePage(0)} disabled={currentPage === 0}>
-                            <MdKeyboardDoubleArrowLeft />
-                        </div>
-                        <div onClick={() => changePage(currentPage - 1)} disabled={currentPage === 0}>
-                            <MdKeyboardArrowLeft />
-                        </div>
-
-                        {getPageRange().map((page) => (
-                            <div
-                                key={page}
-                                className={`${styles.pageBtn} ${currentPage === page ? styles.active : ''}`}
-                                onClick={() => changePage(page)}
-                            >
-                                {page + 1}
-                            </div>
-                        ))}
-
-                        <div onClick={() => changePage(currentPage + 1)} disabled={isLastPage}>
-                            <MdKeyboardArrowRight />
-                        </div>
-                        <div onClick={() => changePage(totalPages - 1)} disabled={isLastPage}>
-                            <MdKeyboardDoubleArrowRight />
-                        </div>
+                {getPageRange().map((page) => (
+                    <div
+                        key={page}
+                        className={`${styles.pageBtn} ${currentPage === page ? styles.active : ''}`}
+                        onClick={() => changePage(page)}
+                    >
+                        {page + 1}
                     </div>
+                ))}
+
+                <div onClick={() => changePage(currentPage + 1)} disabled={isLastPage}>
+                    <MdKeyboardArrowRight/>
+                </div>
+                <div onClick={() => changePage(totalPages - 1)} disabled={isLastPage}>
+                    <MdKeyboardDoubleArrowRight/>
                 </div>
             </div>
-            <Link to={'write'} className={styles.writeButton}>+</Link>
-        </>
-    );
+        </div>
+        </div>) : (<div className={styles.noResult}>검색결과가 없습니다.</div>) }
+    <Link to={'write'} className={styles.writeButton}>+</Link>
+</>
+)
+    ;
 };
 
 export default React.memo(BoardList);
